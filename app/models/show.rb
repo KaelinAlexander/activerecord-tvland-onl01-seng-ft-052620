e1afb5dc3e37eb59e3d0acf952aad5623f107794
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-01 20:10:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,11 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
+def actors_list
+  list = []
+  self.actors.each do |actor|
+    list << actor.full_name
+  end
+end
+
 end
