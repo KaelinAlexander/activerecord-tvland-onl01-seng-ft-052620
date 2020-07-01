1f2671972470cb1b5445d83c1eb31e4c9bb3b1ce
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-01 19:57:34 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -12,7 +12,7 @@ class Actor < ActiveRecord::Base
   def list_roles
     roles = []
       self.characters.each do |character|
-        roles << "#{character.name} - #{character.show}"
+        roles << "character.name - character.show"
       end
     roles
     binding.pry
